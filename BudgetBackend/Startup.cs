34621c626747258f4df2208164a573cc783af926
Ben Loper <loperb06@gmail.com>
2022-06-11 11:19:38 -0400

    Remove automagic migrations

--- BudgetBackend/Startup.cs
+++ BudgetBackend/Startup.cs
@@ -73,8 +73,6 @@ namespace BudgetBackend
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, BudgetAppContext context)
         {
-            context.Database.Migrate();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
