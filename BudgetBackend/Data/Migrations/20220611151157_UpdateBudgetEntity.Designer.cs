6f95cc288beba490f22c54429d4ac8858461894c
Ben Loper <loperb06@gmail.com>
2022-06-11 11:12:14 -0400

    Add ef migration

--- BudgetBackend/Data/Migrations/20220611151157_UpdateBudgetEntity.Designer.cs
+++ BudgetBackend/Data/Migrations/20220611151157_UpdateBudgetEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BudgetBackend.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BudgetBackend.Data.Migrations
 {
     [DbContext(typeof(BudgetAppContext))]
-    partial class BudgetappContextModelSnapshot : ModelSnapshot
+    [Migration("20220611151157_UpdateBudgetEntity")]
+    partial class UpdateBudgetEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BudgetBackend/Data/Migrations/20220611151157_UpdateBudgetEntity.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace BudgetBackend.Data.Migrations
+{
+    public partial class UpdateBudgetEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transaction_Budget",
+                table: "Transaction");
+
+            migrationBuilder.DropTable(
+                name: "WeeklyBudget");
+
+            migrationBuilder.CreateTable(
+                name: "Budget",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    MonthlyIncomeId = table.Column<int>(type: "integer", nullable: false),
+                    Amount = table.Column<decimal>(type: "money", nullable: false),
+                    Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    IsWeekly = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Budget", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Budget_MonthlyIncome",
+                        column: x => x.MonthlyIncomeId,
+                        principalTable: "MonthlyIncome",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Budget",
+                table: "Budget",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WeeklyBudget_MonthlyIncomeId",
+                table: "Budget",
+                column: "MonthlyIncomeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transaction_Budget",
+                table: "Transaction",
+                column: "BudgetId",
+                principalTable: "Budget",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transaction_Budget",
+                table: "Transaction");
+
+            migrationBuilder.DropTable(
+                name: "Budget");
+
+            migrationBuilder.CreateTable(
+                name: "WeeklyBudget",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    MonthlyIncomeId = table.Column<int>(type: "integer", nullable: false),
+                    Amount = table.Column<decimal>(type: "money", nullable: false),
+                    Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeeklyBudget", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Budget_MonthlyIncome",
+                        column: x => x.MonthlyIncomeId,
+                        principalTable: "MonthlyIncome",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Budget",
+                table: "WeeklyBudget",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WeeklyBudget_MonthlyIncomeId",
+                table: "WeeklyBudget",
+                column: "MonthlyIncomeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transaction_Budget",
+                table: "Transaction",
+                column: "BudgetId",
+                principalTable: "WeeklyBudget",
+                principalColumn: "Id");
+        }
+    }
+}
